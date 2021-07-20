d721f1a7040feaa4cd5a62e15311978bd92d5a6f
Bernito <bernitomuko@gmail.com>
2021-07-20 06:56:43 +0300

    added the create record function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <conio.h>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 class STUDENT {
+
 	int regNo;
+	string name;
+	int m, e, k, p, c, b, h;
+	int total;
 
 public:
-	void readData() {
+
+	void dataInput() {
+
+		cout << "Enter the student's Registration no: \n";
+		cin >> regNo;
+		cout << "Enter the student's Name: \n";
+		cin.ignore();
+		getline(cin, name);
+		cout << "Enter the following marks out of 100:\n ";
+		cout << "\nMathematics - "; cin >> m;
+		cout << "\nEnglish - "; cin >> e;
+		cout << "\nKiswahili - "; cin >> k;
+		cout << "\nPhysics - "; cin >> p;
+		cout << "\nChemistry - "; cin >> c;
+		cout << "\nBiology - "; cin >> b;
+		cout << "\nHistory - "; cin >> h;
+	}
+
+	void dataOutput() {
 
 	}
 
@@ -18,7 +41,6 @@ public:
 
 
 private:
-
 };
 
 STUDENT obj;
@@ -27,18 +49,18 @@ void classResults();
 void reportCard();
 bool isReportCard();
 void viewReportCard(const int& x);
+void entryMenu();
+void createRecord();
 
 
 int main() {
-
-	STUDENT obj;
 
 	int choice;
 	cout << "--------------------------------------------------------\n\n";
 	cout << "                       MAIN MENU\n\n";
 	cout << "--------------------------------------------------------\n\n";
 
-	cout << "Please select your option (1-3)\n\n";
+	cout << "Please select your option: \n\n";
 	cout << "\t1. RESULTS MENU"
 		<< "\n\t2. ENTRY/EDIT MENU"
 		<< "\n\t3. EXIT\n\n";
@@ -50,8 +72,9 @@ int main() {
 	case 1:
 		resultsMenu();
 		break;
-		/*case 2:
-			break;*/
+		case 2:
+			entryMenu();
+			break;
 	case 3:
 		exit(0);
 		break;
@@ -98,12 +121,13 @@ void resultsMenu() {
 void classResults() {
 
 	int z;
+	char ch;
 	STUDENT obj;
 	ifstream IFobj("Class Results.txt");
 
 	if (!IFobj) {
 		cout << "\n\t\tRESULTS NOT FOUND!\n";
-		cout << "\t\tPress 0 to redirect...";
+		cout << "\t\tPress 0 to go back to the main menu";
 		cin >> z;
 		if (z == 0) {
 			cout << endl << endl;
@@ -120,6 +144,16 @@ void classResults() {
 		{
 
 		}
+			cout << "\n\n\t\tDo you want to view more results?\n"
+		<< "\t\tPress (Y) - Yes     (N) - No\n";
+	cin >> ch;
+
+	if (ch == 'Y' || ch=='y') {
+		resultsMenu();
+	}
+	if (ch == 'N' || ch=='n') {
+		cout << "\033[2J\033[1;1H";
+		main();
 	}*/
 	IFobj.close();
 }
@@ -174,10 +208,61 @@ void viewReportCard(const int &x) {
 
 		if (obj.regNoRet() == x) {
 			cout << "\033[2J\033[1;1H";
-			obj.readData();
+			obj.dataOutput();
 		}
 	}
 	IFobj.close();
+	
+}
+
+void entryMenu() {
+
+	int choice;
+	cout << "\033[2J\033[1;1H";
+
+	cout << "--------------------------------------------------------\n\n";
+	cout << "                      ENTRY MENU\n\n";
+	cout << "--------------------------------------------------------\n\n";
+
+	cout << "Select your option: \n"
+		 << "\n\t\t1. CREATE STUDENT RECORD \n\t\t2. VIEW ALL STUDENTS' RECORDS \n\t\t3. SEARCH STUDENT RECORD \n\t\t4. MODIFY STUDENT RECORD"
+		 << "\n\t\t5. DELETE STUDENT RECORD \n\t\t6. BACK\n\n";
+		
+	cin >> choice;
+		switch (choice) {
+
+			case 1:
+				cout << "\033[2J\033[1;1H";
+				createRecord();
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				cout << "\033[2J\033[1;1H";
+				main();
+				break;
+		}
+}
+
+void createRecord() {
+
+	int back;
+	ofstream OFobj("Report card.dat");
+	obj.dataInput();
+	OFobj.write(reinterpret_cast<char*> (&obj), sizeof(STUDENT));		
+	OFobj.close();
+
+	cout << "\n\nStudent information succesfully added!\n" << "Press 0 to go back to the main menu...\n";
+	cin >> back;
+	if (back == 0)
+		cout << "\033[2J\033[1;1H";
+		main();
 	
 }
 
