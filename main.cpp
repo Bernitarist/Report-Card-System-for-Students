75964f226ef47025d04d774472ac094dc57286c3
Bernito <bernitomuko@gmail.com>
2021-07-20 13:26:54 +0300

    removed class

--- main.cpp
+++ main.cpp
@@ -5,53 +5,19 @@
 
 using namespace std;
 
-class STUDENT {
 
-	int regNo;
-	string name;
-	int m, e, k, p, c, b, h;
-	int total;
-
-public:
-
-	void dataInput() {
-		
-			cout << "Enter the student's Registration no: \n";
-			cin >> regNo;
-			cout << "Enter the student's Name: \n";
-			cin.ignore();
-			getline(cin, name);
-			cout << "Enter the following marks out of 100:\n ";
-			cout << "\nMathematics - "; cin >> m;
-			cout << "\nEnglish - "; cin >> e;
-			cout << "\nKiswahili - "; cin >> k;
-			cout << "\nPhysics - "; cin >> p;
-			cout << "\nChemistry - "; cin >> c;
-			cout << "\nBiology - "; cin >> b;
-			cout << "\nHistory - "; cin >> h;
-		
-	}
-
-	void dataOutput() {
-
-	}
-
-	int regNoRet() {
-		return regNo;
-	}
-
-
-private:
-};
-
-STUDENT obj;
 void resultsMenu();
 void classResults();
 void reportCard();
-bool isReportCard();
-void viewReportCard(const int& x);
 void entryMenu();
 void createRecord();
+void dispAll();
+bool isReportCard(const int& x);
+void dataOutput();
+int regNoRet();
+void calculate();
+void tableResults();
+void dataInput();
 
 
 int main() {
@@ -73,9 +39,9 @@ int main() {
 	case 1:
 		resultsMenu();
 		break;
-		case 2:
-			entryMenu();
-			break;
+	case 2:
+		entryMenu();
+		break;
 	case 3:
 		exit(0);
 		break;
@@ -84,7 +50,6 @@ int main() {
 		main();
 
 	}
-
 	return 0;
 }
 
@@ -117,19 +82,17 @@ void resultsMenu() {
 		cout << "\033[2J\033[1;1H";
 		main();
 	}
-
 }
 
 void classResults() {
 
 	int z;
 	char ch;
-	STUDENT obj;
 	ifstream IFobj("Class Results.txt");
 
 	if (!IFobj) {
 		cout << "\n\t\tRESULTS NOT FOUND!\n";
-		cout << "\t\tPress 0 to go back to the main menu";
+		cout << "\t\tPress 0 to go back to the main menu\n";
 		cin >> z;
 		if (z == 0) {
 			cout << "\033[2J\033[1;1H";
@@ -142,7 +105,7 @@ void classResults() {
 	*   cout << "--------------------------------------------------------\n\n";
 		cout << "                       CLASS RESULTS \n\n";
 		cout << "--------------------------------------------------------\n\n";
-		while (IFobj.read(reinterpret_cast<char*>(&obj), sizeof(STUDENT)))
+		while (IFobj)
 		{
 
 		}
@@ -172,11 +135,12 @@ void reportCard() {
 	cout << "\tEnter the Student's Registration Number:\n";
 
 	cin >> regNo;
-	bool status = isReportCard();
-	if(status){ 
-		viewReportCard(regNo);
+	bool status = isReportCard(regNo);
+	if (status) {
+		dataOutput();
 	}
 	
+		
 	cout << "\n\n\t\tDo you want to view another student's report?\n"
 		<< "\t\tPress (Y) - Yes     (N) - No\n";
 	cin >> ch;
@@ -187,35 +151,9 @@ void reportCard() {
 	if (ch == 'N' || ch=='n') {
 		cout << "\033[2J\033[1;1H";
 		main();
-	}
-	
+	}	
 }
 
-bool isReportCard() {
-
-	ifstream IFobj("Report Card.dat");
-	if (IFobj) {
-		return true;
-	}
-	else {
-		cout << "\t\t\tFILE CANNOT BE FOUND!\n\n";
-	}
-	IFobj.close();
-}
-
-void viewReportCard(const int &x) {
-
-	ifstream IFobj("Report Card.dat");
-	while (IFobj.read(reinterpret_cast<char*>(&obj), sizeof(STUDENT))) {
-
-		if (obj.regNoRet() == x) {
-			cout << "\033[2J\033[1;1H";
-			obj.dataOutput();
-		}
-	}
-	IFobj.close();
-	
-}
 
 void entryMenu() {
 
@@ -227,7 +165,7 @@ void entryMenu() {
 	cout << "--------------------------------------------------------\n\n";
 
 	cout << "Select your option: \n"
-		 << "\n\t\t1. CREATE STUDENT RECORD \n\t\t2. VIEW ALL STUDENTS' RECORDS \n\t\t3. SEARCH STUDENT RECORD \n\t\t4. MODIFY STUDENT RECORD"
+		 << "\n\t\t1. CREATE STUDENT RECORD \n\t\t2. VIEW ALL STUDENTS RECORDS \n\t\t3. SEARCH STUDENT RECORD \n\t\t4. MODIFY STUDENT RECORD"
 		 << "\n\t\t5. DELETE STUDENT RECORD \n\t\t6. BACK\n\n";
 		
 	cin >> choice;
@@ -238,6 +176,8 @@ void entryMenu() {
 				createRecord();
 				break;
 			case 2:
+				cout << "\033[2J\033[1;1H";
+				dispAll();
 				break;
 			case 3:
 				break;
@@ -253,15 +193,9 @@ void entryMenu() {
 }
 
 void createRecord() {
-
+	dataInput();
 	int back;
-
-	ofstream OFobj("Report card.dat");
-	obj.dataInput();
-	OFobj.write(reinterpret_cast<char*> (&obj), sizeof(STUDENT));	
-	OFobj.close();
-
-	cout << "\n\nStudent information succesfully added!\n" << "Press 1 to add a student or 0 to go back to the main menu...\n";
+	cout << "\n\n\t\tStudent information succesfully added!\n" << "\t\tPress 1 to add a student or 0 to go back to the main menu...\n";
 	cin >> back;
 
 		switch (back) {
@@ -274,5 +208,128 @@ void createRecord() {
 			createRecord();
 		}	
 }
+
+void dispAll() {
+
+	ifstream IFobj("Report card.txt");
+	if (!IFobj) {
+		cout << "\033[2J\033[1;1H";
+		cout << "\n\t\tFile cannot be found!!\n\n";
+		cout << endl << endl;
+		main();
+	}	
+
+	cout << "--------------------------------------------------------\n\n";
+	cout << "                    STUDENTS RECORDS\n\n";
+	cout << "--------------------------------------------------------\n\n";
+
+	for (int x = 1;x<=(IFobj.eof();x++) {
+
+		dataOutput();
+	}
+	
+}
+
+bool isReportCard(const int &x) {
+	ifstream IFobj("Report Card.txt");
+	if (IFobj)
+		return true;
+}
+
+void dataOutput() {
+	int regNo;
+	string name;
+	int m, e, k, p, c, b, h;
+	int total;
+	float ave;
+	char grade;
+
+	ifstream IFobj("Report Card.txt");
+	if (!IFobj) {
+		cout << "\n\t\tFILE COULD NOT BE FOUND!\n\n";
+	}
+	
+		IFobj >> regNo >> name >> m >> e >> k >> p >> b >> c >> h >> total >> ave >> grade;
+			cout << "Student Registration number: " << regNo << endl;
+			cout << "Student Name: " << name << endl;
+			cout << "Mathematics: " << m << endl;
+			cout << "English: " << e << endl;
+			cout << "Kiswahili: " << k << endl;
+			cout << "Physics: " << p << endl;
+			cout << "Biology: " << b << endl;
+			cout << "Chemistry : " << c << endl;
+			cout << "History: " << h << endl;
+			calculate();
+			cout << "Total " << total << endl;
+			cout << "Average " << ave << endl;
+			cout << "Grade " << grade << endl;
+		
+	IFobj.close();
+}
+
+void dataInput() {
+	int regNo;
+	string name;
+	int m, e, k, p, c, b, h;
+	int total;
+	float ave;
+	char grade;
+
+	cout << "Enter the student's Registration no: \n";
+	cin >> regNo;
+	cout << "Enter the student's Name: \n";
+	cin.ignore();
+	getline(cin, name);
+	cout << "Enter the following marks out of 100:\n ";
+	cout << "\nMathematics - "; cin >> m;
+	cout << "\nEnglish - "; cin >> e;
+	cout << "\nKiswahili - "; cin >> k;
+	cout << "\nPhysics - "; cin >> p;
+	cout << "\nBiology - "; cin >> b;
+	cout << "\nChemistry - "; cin >> c;
+	cout << "\nHistory - "; cin >> h;
+
+	ofstream OFobj("Report card.txt");
+	OFobj << regNo << name << m  << e  << k << p << b << c << h;
+	OFobj.close();
+}
+
+
+void tableResults() {
+
+}
+
+void calculate() {
+	int regNo;
+	string name;
+	int m, e, k, p, c, b, h;
+	int total;
+	float ave;
+	char grade;
+
+	total = m + e + k + p + b + c + h;
+	ave = total / 7.0;
+
+	if (total >= 560)
+		grade = 'A+';
+	if (total >= 490 && total < 560)
+		grade = 'A';
+	if (total >= 420 && total < 490)
+		grade = 'B';
+	if (total >= 350 && total < 420)
+		grade = 'C';
+	if (total > 280 && total < 350)
+		grade = 'D';
+	if (total <= 280)
+		grade = 'E';
+}
+
+int regNoRet() {
+
+	return regNoRet;
+}
+
+
+
 
 
